886f9df68578c6a3780afe4cbb8ad9677f8fd992
Hanyun-75 <ucfnhh0@ucl.ac.uk>
2026-02-23 09:41:49 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  //GitHub Pages: https://Hanyun-75.github.io/casa0028Assessment1/
+  
   base: "/casa0028Assessment1/",
   plugins: [
     react({
